f0c6e4ec24c7627517623917a3fecc665c96afbc
alchemistake <alchemistake@gmail.com>
2020-08-06 20:21:11 +0200

    Waves Done!

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Welcome.scss';
 import Wallpaper from './Wallpaper/Wallpaper'
+import {ReactComponent as BackWaves} from './BackWaves.svg';
+import {ReactComponent as FrontWaves} from './FrontWaves.svg';
 
 class Welcome extends React.Component {
     render() {
-        return <Wallpaper/>;
+        return <div>
+            <Wallpaper/>
+            <div className="footer">
+                <BackWaves></BackWaves>
+                <FrontWaves></FrontWaves>
+                <div className="margin"/>
+            </div>
+        </div>;
     }
 }
 
