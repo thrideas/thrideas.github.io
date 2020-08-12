0156637c9fd842bb602a554cfab46f25dfdd4416
alchemistake <alchemistake@gmail.com>
2020-08-12 21:16:58 +0200

    Tagline Text is more readable now

--- src/components/Welcome/TagLine/TagLine.js
+++ src/components/Welcome/TagLine/TagLine.js
@@ -3,8 +3,8 @@ import './TagLine.scss';
 
 class TagLine extends React.Component {
     render() {
-        return <div className="left">
-            <p>Hello World! This is</p>
+        return <div className="tag-line">
+            <p>It's a me:</p>
             <img src="logo-fill.png" className="logo" alt="Logo"/>
             <p>I'm Infrastructure Engineer by day.</p>
             <p>I try to be Game Developer by night.</p>
