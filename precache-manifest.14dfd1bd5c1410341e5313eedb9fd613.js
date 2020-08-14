6c8b2a8d65d6d893237ca2ab3ab7e4b20d989774
alchemistake <alchemistake@gmail.com>
2020-08-14 22:23:24 +0200

    Updates

---
+++ precache-manifest.14dfd1bd5c1410341e5313eedb9fd613.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a1585baa9fa854761347b636d15559",
+    "url": "/alchemistake/index.html"
+  },
+  {
+    "revision": "ef2b6f2502e54a8ec4b9",
+    "url": "/alchemistake/static/css/main.8653db3b.chunk.css"
+  },
+  {
+    "revision": "3db5ce83030c9ca9e759",
+    "url": "/alchemistake/static/js/2.274d3523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alchemistake/static/js/2.274d3523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2b6f2502e54a8ec4b9",
+    "url": "/alchemistake/static/js/main.6be4dd43.chunk.js"
+  },
+  {
+    "revision": "0f649f7f53533b8b8060",
+    "url": "/alchemistake/static/js/runtime-main.a4a6a234.js"
+  },
+  {
+    "revision": "99deb32d50adaf04c30884ee311fd8c2",
+    "url": "/alchemistake/static/media/BackWaves.99deb32d.svg"
+  },
+  {
+    "revision": "cb7a850d82e9bb099b1b994c7d64b4f0",
+    "url": "/alchemistake/static/media/CreteRound-Regular.cb7a850d.ttf"
+  },
+  {
+    "revision": "cbe54824c4c55a7bb8ab29fe8fa5a186",
+    "url": "/alchemistake/static/media/DownChevron.cbe54824.svg"
+  },
+  {
+    "revision": "619e9ea0905da76ca896cd4edc17cfa8",
+    "url": "/alchemistake/static/media/FrontWaves.619e9ea0.svg"
+  }
+]);
