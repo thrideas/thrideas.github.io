c9467e8fce96bd482bf84af221cff129a8526f5a
alchemistake <alchemistake@gmail.com>
2020-08-14 22:15:58 +0200

    Under construction text.

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import './App.scss';
 import Welcome from "./Welcome/Welcome";
 
 function App() {
-  return (
-      <Welcome/>
-  );
+    return (
+        <div>
+            <Welcome/>
+            <h1>
+                Portfolio will be here soon! Working on it!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
