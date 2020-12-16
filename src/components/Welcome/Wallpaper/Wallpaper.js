c31dc0ffaddbf5ef5a80cc2355eab5168d06babf
caner caliskaner <alchemistake@gmail.com>
2020-12-16 22:36:52 +0100

    Background resizing done! Flashing Fixed!

--- src/components/Welcome/Wallpaper/Wallpaper.js
+++ src/components/Welcome/Wallpaper/Wallpaper.js
@@ -3,10 +3,13 @@ import './Wallpaper.scss';
 
 class Wallpaper extends React.Component {
     render() {
-        return <div className="wallpaper">
-            <video autoPlay playsInline muted loop id="logo">
-                <source src="logo.mp4" type="video/mp4"/>
-            </video>
+        return <div>
+            <div className="wallpaper flash"></div>
+            <div className="wallpaper">
+                <video autoPlay playsInline muted loop id="logo">
+                    <source src="logo.mp4" type="video/mp4"/>
+                </video>
+            </div>
         </div>;
     }
 }
